e089b8b169e060c8f125ffe58894c00f05e4748d
Jeremiah Sheehy - Yum <jxs7852@system76-pc.localdomain>
2025-02-16 22:13:27 -0600

    change order generation into an interface

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 
 ### Changes
 - Added math/js
+- Added an interface for order generation
+- Added item weight to the item object (see TODO on fixing this)
+
+### Todo
+- Figure out a better weight to handle individual items w/ weights and groups of items w/ weights (intefaces)
--- js/interfaces.js
+++ js/interfaces.js
@@ -33,9 +33,53 @@ class Order {
     }
 }
 
+// TODO: Unwind this object from weight calcs
 class Item {
-    constructor(productId, quantity = 1) {
+    constructor(productId, quantity = 1, totalWeight = 0) {
         this.productId = productId;
         this.quantity = quantity;
+        this.totalWeight = totalWeight;
     }
+}
+
+class IOrderGenerator {
+    constructor(products) {
+        this.products = products;
+        this.minUniqueItemsPerOrder = 1;
+        this.maxUniqueItemsPerOrder = 5;
+        this.minItemQuantity = 1;
+        this.maxItemQuantity = 12;
+    }
+
+    setNumberOfUniqueItemsRange(min, max) {
+        if (min > max) {
+            min = max;
+            throw new Error('dont be stupid, min > max');
+        }
+        this.minUniqueItemsPerOrder = min;
+        this.maxUniqueItemsPerOrder = max;
+    }
+
+    setItemQuantityRange(min, max) {
+        if (min > max) {
+            min = max;
+            throw new Error('dont be stupid, min > max');
+        }
+        this.minItemQuantity = min
+        this.maxItemQuantity = max;
+    }
+
+    generateCompleteOrder(batchSize = 1, minItems = 1) {
+        throw new Error('Not implemented');
+    }
+
+    generateIncompleteOrder() {
+        throw new Error('Not implemented');
+    }
+
+    // uniform random distribution
+    randomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
 }
